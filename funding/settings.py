e66be8c95bcf61b17f6be528fec38a7b9bf24697
unknown <naitikpatel107@gmail.com>
2023-03-31 22:00:21 +0530

    mail features added

--- funding/settings.py
+++ funding/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ahmed1077mehta@gmail.com'
-EMAIL_HOST_PASSWORD = 'pgpziaogvvrygxii'
+# EMAIL_HOST_PASSWORD = '#'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
