cdee42edf2857ee91a0d6178bb05ef50eb3a91f4
unknown <naitikpatel107@gmail.com>
2023-04-03 20:11:23 +0530

    mail feature added commit

--- authen/models.py
+++ authen/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class user(models.Model):
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True)
+
